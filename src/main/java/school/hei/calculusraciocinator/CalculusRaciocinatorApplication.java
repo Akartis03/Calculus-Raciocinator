5fabd4fe03f43ceefa11fd2ab050f24b560aa29f
RSitrakaFL <hei.sitraka@gmail.com>
2024-06-07 14:45:11 +0300

    Initial commit

---
+++ src/main/java/school/hei/calculusraciocinator/CalculusRaciocinatorApplication.java
@@ -0,0 +1,13 @@
+package school.hei.calculusraciocinator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalculusRaciocinatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CalculusRaciocinatorApplication.class, args);
+    }
+
+}
